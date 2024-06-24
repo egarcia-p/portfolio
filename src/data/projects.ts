b69cfd9f45a8112d1615cb95b4b9f7bee214143a
egarcia-p <eugenio5039@gmail.com>
2024-06-23 21:24:15 -0500

    Add / new projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,20 +7,20 @@ export type Project = {
 
 const projects: Project[] = [
   {
+    title: "CoupleCents App",
+    techs: ["NextJS", "TailwindCSS", "PostgreSQL","Drizzle ORM", "Vercel"],
+    link: "https://github.com/egarcia-p/couple-cents",
+    isComingSoon: false,
+  },
+  {
     title: "Recipe App",
     techs: ["ReactJS", "Ruby on Rails", "zod"],
     link: "https://github.com",
     isComingSoon: true,
   },
   {
-    title: "Expense App",
-    techs: ["ReactJS", "NodeJS"],
-    link: "https://",
-    isComingSoon: true,
-  },
-  {
-    title: "Portfolio",
-    techs: ["Astro"],
+    title: "Javamex",
+    techs: ["Astro", "TailwindCSS"],
     link: "/",
     isComingSoon: true,
   },
